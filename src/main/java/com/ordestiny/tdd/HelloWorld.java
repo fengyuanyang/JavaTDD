d87ca54a97b84542f171ff0a4b28dc6f35c1d30b
fengyuanyang <apple@Owende-Mac-mini.local>
2020-10-07 01:03:53 +0800

    JavaJdd#1 1. add HelloWorld and Unit Test 2. add gradle file - build.gradle

---
+++ src/main/java/com/ordestiny/tdd/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.ordestiny.tdd;
+
+public class HelloWorld {
+    String hello() {
+        return "Hello World Java";
+    }
+}
---
+++ src/test/java/com/ordestiny/tdd/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.ordestiny.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/* author fengyuan yang coolsealtw@hotmail.com
+ */
+class HelloWorldTest {
+    private HelloWorld helloWorld = new HelloWorld();
+
+    @Test
+    void test_hello_expect_hello_world_java() {
+        String result = helloWorld.hello();
+        assertEquals("Hello World Java", result);
+    }
+}
