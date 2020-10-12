d4176ea64a11c7c58e97c7e292e2a122badd8321
fengyuanyang <apple@Owende-Mac-mini.local>
2020-10-13 01:29:52 +0800

    TDD on While Loop

---
+++ src/test/java/loop/WhileLoopTest.java
@@ -0,0 +1,34 @@
+package loop;
+
+import com.ordestiny.tdd.loop.WhileLoop;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WhileLoopTest {
+    private WhileLoop loop = new WhileLoop();
+
+    @Test
+    void doWhile_TenTimes_ReturnTen() {
+        int result = loop.doWhile(10);
+        assertEquals(10, result);
+    }
+
+    @Test
+    void doWhile_ZeroTimes_ReturnOne() {
+        int result = loop.doWhile(0);
+        assertEquals(1, result);
+    }
+
+    @Test
+    void whileTimes_ZeroTimes_ReturnZero() {
+        int result = loop.whileTimes(0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    void whileTimes_TenTimes_ReturnTen() {
+        int result = loop.whileTimes(10);
+        assertEquals(10, result);
+    }
+}
