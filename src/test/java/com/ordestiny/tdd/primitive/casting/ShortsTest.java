f33b816366a8bd3c51ecfa872eebb8121d13d9db
Matheus Henrique de Sousa Cruz <matheus.cruz@radixeng.com.br>
2020-10-13 15:13:05 -0300

    Adding exception handler tests

--- src/test/java/com/ordestiny/tdd/primitive/casting/ShortsTest.java
+++ src/test/java/com/ordestiny/tdd/primitive/casting/ShortsTest.java
@@ -20,10 +20,28 @@ public class ShortsTest {
     }
 
     @Test
+    public void from_receivingAnIllegalChar_ShouldThrowsAnIllegalArgumentException() {
+
+        char positive = 32999;
+        Assertions.assertThatThrownBy(() -> Shorts.from(positive)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     public void from_receivingAnInt_ShouldReturnAShort() {
         int ninety = 90;
         short ninetyAShort = Shorts.from(ninety);
         Assertions.assertThat(ninetyAShort).isInstanceOf(java.lang.Short.class);
+    }
+
+    @Test
+    public void from_receivingAnIllegalInt_ShouldThrowsAnIllegalArgumentException() {
+
+        int positive = 3203302;
+        int negative = -3203302;
+
+        Assertions.assertThatThrownBy(() -> Shorts.from(positive)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> Shorts.from(negative)).isInstanceOf(IllegalArgumentException.class);
+
     }
 
     @Test
@@ -34,10 +52,31 @@ public class ShortsTest {
     }
 
     @Test
+    public void from_receivingAnIllegalLong_ShouldThrowsAnIllegalArgumentException() {
+
+        long positive = 321321321321L;
+        long negative = -213232132131L;
+
+        Assertions.assertThatThrownBy(() -> Shorts.from(positive)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> Shorts.from(negative)).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
     public void from_receivingAFloat_ShouldReturnAShort() {
         float fifty = 50;
         short fiftyAsShort = Shorts.from(fifty);
         Assertions.assertThat(fiftyAsShort).isInstanceOf(java.lang.Short.class);
+    }
+
+    @Test
+    public void from_receivingAnIllegalFloat_ShouldReturnAFloat() {
+
+        float number = 123221.02f;
+        float otherNumber = -129334.34f;
+
+        Assertions.assertThatThrownBy(() -> Shorts.from(number)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> Shorts.from(otherNumber)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -47,4 +86,12 @@ public class ShortsTest {
         Assertions.assertThat(seventyAsShort).isInstanceOf(java.lang.Short.class);
     }
 
+    @Test
+    public void from_receivingAIllegalDouble_ShouldThrowsAnIllegalArgumentException() {
+
+        double number = 13321321.0;
+        double otherNumber = 1232329.93;
+        Assertions.assertThatThrownBy(() -> Shorts.from(number)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> Shorts.from(otherNumber)).isInstanceOf(IllegalArgumentException.class);
+    }
 }
