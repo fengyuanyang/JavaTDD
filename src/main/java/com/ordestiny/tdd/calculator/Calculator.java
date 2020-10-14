970aadeb4df7f7d2f679e150f55fb4af864e5e47
Barath Tirumala <btirumala@Baraths-MacBook-Pro.local>
2020-10-13 20:50:54 -0500

    calculator object

---
+++ src/main/java/com/ordestiny/tdd/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.ordestiny.tdd.calculator;
+
+public class Calculator {
+
+    public int add(int x, int y){
+        return x+y;
+    }
+
+    public int minus(int x, int y){
+        return x-y;
+    }
+
+    public int multiply(int x, int y){
+        return x*y;
+    }
+
+    public int divide(int x, int y){
+        return x/y;
+    }
+}
---
+++ src/test/java/com/ordestiny/tdd/calculator/CalculatorTest.java
@@ -0,0 +1,40 @@
+package com.ordestiny.tdd.calculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+    int testInteger1 = 50;
+    int testInteger2 = 10;
+
+    @Test
+    void testAdd() {
+        assertEquals(testInteger1 + testInteger2, calculator.add(testInteger1, testInteger2));
+    }
+
+    @Test
+    void testMinus() {
+        assertEquals(testInteger1 - testInteger2, calculator.minus(testInteger1, testInteger2));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(testInteger1 / testInteger2, calculator.divide(testInteger1, testInteger2));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(testInteger1 * testInteger2, calculator.multiply(testInteger1, testInteger2));
+    }
+
+    @Test
+    void testDivideInvalid() {
+        Assertions.assertThatThrownBy(() -> {
+            calculator.divide(10,0);
+        });
+    }
+}
