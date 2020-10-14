9b761a43c088369611ac047c1222a25810993e3f
Aarushi Gupta <54365212+Aarushi5g@users.noreply.github.com>
2020-10-15 00:00:34 +0530

    Updated indentation

--- src/main/java/com/ordestiny/tdd/conditional/Statements.java
+++ src/main/java/com/ordestiny/tdd/conditional/Statements.java
@@ -4,41 +4,37 @@ import java.util.Scanner;
 
 public class Statements {
 
+    /**
+     *  If the number lies between 10 and 99999
+     *  then the method returns the number of digits 
+     *
+     * @param number
+     * @return String
+     */
+    public String ifElseIf(int number) {
+        if (number < 100 && number >= 10) {
+            return ("2 digit number");
+        } else if (number < 1000 && number >= 100) {
+            return ("3 digit number");
+        } else if (number < 10000 && number >= 1000) {
+            return ("4 digit number");
+        } else if (number < 100000 && number >= 10000) {
+            return ("5 digit number");
+        } else {
+            return ("Number is not between 10 & 99999");
+        }
+    }
+
+    public void switchStatement(Double number1, char operator, Double number2) {
+
         /**
-         *  If the number lies between 10 and 99999
-         *  then the method returns the number of digits 
+         *  Selects operator using switch 
+         *  to perform the operation and get the result
          *
-         * @param number
-         * @return String
+         * @param number1
+         * @param operator
+         * @param number2
          */
-     	public String ifElseIf(int number){
-		if(number <100 && number>=10) {
-			return ("2 digit number");
-		}
-		else if(number <1000 && number>=100) {
-			return ("3 digit number");
-		}
-		else if(number <10000 && number>=1000) {
-			return ("4 digit number");
-		}
-		else if(number <100000 && number>=10000) {
-			return ("5 digit number");			
-		}
-		else {
-			return ("Number is not between 10 & 99999");			
-		}
-	}
-
-	public void switchStatement(Double number1, char operator, Double number2) {
-
-        /**
-        *  Selects operator using switch 
-        *  to perform the operation and get the result
-        *
-        * @param number1
-        * @param operator
-        * @param number2
-        */
         Double result;
 
         switch (operator) {
@@ -68,5 +64,5 @@ public class Statements {
                 break;
         }
 
-	}
+    }
 }
