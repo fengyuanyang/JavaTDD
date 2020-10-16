fcc69d56bd488d01124ec29ee334939d2b9e8385
janus.lin <janus.lin@eddidfintech.com>
2020-10-16 09:42:17 +0800

    Update WhileLoopBreak logic

--- src/main/java/com/ordestiny/tdd/loop/WhileLoopBreak.java
+++ src/main/java/com/ordestiny/tdd/loop/WhileLoopBreak.java
@@ -5,17 +5,15 @@ public class WhileLoopBreak {
    * Given 1 int parameter for loops (break) And there is a counter to count the execution times And
    * loop should execute 10 times at most.
    *
-   * @param expectedCount
-   * @return
+   * @param breakAt int
+   * @return int
    */
-  public int loopBreak(int expectedCount) {
-    if (expectedCount < 0) {
-      return 10;
-    }
-
+  public int loopBreak(int breakAt) {
     int count = 0;
-    while (expectedCount > 0 && count < 10) {
-      expectedCount--;
+    while (count < 10) {
+      if (breakAt == count) {
+        break;
+      }
       count++;
     }
     return count;
