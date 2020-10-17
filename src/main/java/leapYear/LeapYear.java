cebc657ad46ca16d4e7e557aef3a685965e0e2d7
ipapast <6989529+ipapast@users.noreply.github.com>
2020-10-17 13:48:53 +0100

    Add function to calculate if given year is a leap year and add unit test (#65)

---
+++ src/main/java/leapYear/LeapYear.java
@@ -0,0 +1,8 @@
+package leapYear;
+
+public class LeapYear {
+    public static boolean isLeapYear(int year) {
+        assert year >= 1583; // not valid before this date.
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    }
+}
