0a4e2a4a47344dc0aa1d8d167fe72c311267d2fc
vicbl <victor.blanchard@telecom-st-etienne.fr>
2020-10-18 13:39:57 +0200

    update indentation

--- src/test/java/com/ordestiny/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/ordestiny/tdd/fizzbuzz/FizzBuzzTest.java
@@ -11,52 +11,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
 
-    FizzBuzz fizzBuzz = new FizzBuzz();
-    private final PrintStream standardOut = System.out;
-    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+  FizzBuzz fizzBuzz = new FizzBuzz();
+  private final PrintStream standardOut = System.out;
+  private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
-    @BeforeEach
-    public void setUp() {
-        System.setOut(new PrintStream(outputStreamCaptor));
-    }
+  @BeforeEach
+  public void setUp() {
+    System.setOut(new PrintStream(outputStreamCaptor));
+  }
 
-    @AfterEach
-    public void tearDown() {
-        System.setOut(standardOut);
-    }
+  @AfterEach
+  public void tearDown() {
+    System.setOut(standardOut);
+  }
 
-    @Test
-    public void printFizzBuzz_given15Iterations_fizzBuzzIsCorrectlyPrinted() {
-        fizzBuzz.printFizzBuzz(15);
-        assertEquals("1\r\n" +
-                "2\r\n" +
-                "Fizz\r\n" +
-                "4\r\n" +
-                "Buzz\r\n" +
-                "Fizz\r\n" +
-                "7\r\n" +
-                "8\r\n" +
-                "Fizz\r\n" +
-                "Buzz\r\n" +
-                "11\r\n" +
-                "Fizz\r\n" +
-                "13\r\n" +
-                "14\r\n" +
-                "FizzBuzz", outputStreamCaptor.toString().trim());
-    }
+  @Test
+  public void printFizzBuzz_given15Iterations_fizzBuzzIsCorrectlyPrinted() {
+    fizzBuzz.printFizzBuzz(15);
+    assertEquals("1\r\n" +
+      "2\r\n" +
+      "Fizz\r\n" +
+      "4\r\n" +
+      "Buzz\r\n" +
+      "Fizz\r\n" +
+      "7\r\n" +
+      "8\r\n" +
+      "Fizz\r\n" +
+      "Buzz\r\n" +
+      "11\r\n" +
+      "Fizz\r\n" +
+      "13\r\n" +
+      "14\r\n" +
+      "FizzBuzz", outputStreamCaptor.toString().trim());
+  }
 
-    @Test
-    public void fizzBuzz_givenMultipleOfThree_fizzReturned() {
-        assertEquals("Fizz", fizzBuzz.fizzBuzz(3));
-    }
+  @Test
+  public void fizzBuzz_givenMultipleOfThree_fizzReturned() {
+    assertEquals("Fizz", fizzBuzz.fizzBuzz(3));
+  }
 
-    @Test
-    public void fizzBuzz_givenMultipleOfFive_buzzReturned() {
-        assertEquals("Buzz", fizzBuzz.fizzBuzz(5));
-    }
+  @Test
+  public void fizzBuzz_givenMultipleOfFive_buzzReturned() {
+    assertEquals("Buzz", fizzBuzz.fizzBuzz(5));
+  }
 
-    @Test
-    public void fizzBuzz_givenMultipleOfThreeAndFive_fizzBuzzReturned() {
-        assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(15));
-    }
+  @Test
+  public void fizzBuzz_givenMultipleOfThreeAndFive_fizzBuzzReturned() {
+    assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(15));
+  }
 }
