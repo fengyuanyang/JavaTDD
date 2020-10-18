43adef3de56ea71d3ada0d1479e7014519a10c52
vicbl <victor.blanchard@telecom-st-etienne.fr>
2020-10-18 13:37:09 +0200

    update indentation

--- src/main/java/com/ordestiny/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/ordestiny/tdd/fizzbuzz/FizzBuzz.java
@@ -2,25 +2,25 @@ package com.ordestiny.tdd.fizzbuzz;
 
 public class FizzBuzz {
 
-    public void printFizzBuzz(int numberIteration) {
-        for (int i = 1; i <= numberIteration; i++) {
-            System.out.println(fizzBuzz(i));
-        }
+  public void printFizzBuzz(int numberIteration) {
+    for (int i = 1; i <= numberIteration; i++) {
+      System.out.println(fizzBuzz(i));
     }
+  }
 
-    public String fizzBuzz(int i) {
-        boolean isFizz = i % 3 == 0;
-        boolean isBuzz = i % 5 == 0;
-        boolean isFizzBuzz = isFizz && isBuzz;
+  public String fizzBuzz(int i) {
+    boolean isFizz = i % 3 == 0;
+    boolean isBuzz = i % 5 == 0;
+    boolean isFizzBuzz = isFizz && isBuzz;
 
-        if (isFizzBuzz) {
-            return "FizzBuzz";
-        } else if (isFizz) {
-            return "Fizz";
-        } else if (isBuzz) {
-            return "Buzz";
-        } else {
-            return String.valueOf(i);
-        }
+    if (isFizzBuzz) {
+      return "FizzBuzz";
+    } else if (isFizz) {
+      return "Fizz";
+    } else if (isBuzz) {
+      return "Buzz";
+    } else {
+      return String.valueOf(i);
     }
+  }
 }
