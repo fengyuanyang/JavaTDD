15bd5499e1e6e48697ab0cc3a05c92bbe71d9f9b
liun03 <liun03@gmail.com>
2020-10-28 00:17:21 +0800

    implementation

--- src/main/java/com/ordestiny/tdd/kata/FooBarQix.java
+++ src/main/java/com/ordestiny/tdd/kata/FooBarQix.java
@@ -1,9 +1,44 @@
 package com.ordestiny.tdd.kata;
 
+import java.math.BigDecimal;
+
 public class FooBarQix {
 
-  public static String compute(String number) {
-    return null;
-  }
+  private static final BigDecimal THREE = new BigDecimal(3);
+  private static final BigDecimal FIVE = new BigDecimal(5);
+  private static final BigDecimal SEVEN = new BigDecimal(7);
+  private static final String FOO = "Foo";
+  private static final String BAR = "Bar";
+  private static final String QIX = "Qix";
 
+
+  public static String compute(String input) {
+    String result = "";
+    BigDecimal number = new BigDecimal(input);
+    if (number.divideAndRemainder(THREE)[1].compareTo(BigDecimal.ZERO) == 0) {
+      result += FOO;
+    }
+    if (number.divideAndRemainder(FIVE)[1].compareTo(BigDecimal.ZERO) == 0) {
+      result += BAR;
+    }
+    if (number.divideAndRemainder(SEVEN)[1].compareTo(BigDecimal.ZERO) == 0) {
+      result += QIX;
+    }
+    String computeFBQNumber = "";
+    while (number.compareTo(BigDecimal.ZERO) > 0) {
+      BigDecimal[] divide10 = number.divideAndRemainder(new BigDecimal(10));
+      if (divide10[1].compareTo(THREE) == 0) {
+        computeFBQNumber = FOO + computeFBQNumber;
+      }
+      if (divide10[1].compareTo(FIVE) == 0) {
+        computeFBQNumber = BAR + computeFBQNumber;
+      }
+      if (divide10[1].compareTo(SEVEN) == 0) {
+        computeFBQNumber = QIX + computeFBQNumber;
+      }
+      number = divide10[0];
+    }
+    result += computeFBQNumber;
+    return result.length() == 0 ? input : result;
+  }
 }
