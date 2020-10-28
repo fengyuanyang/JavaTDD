01387bcb7da278e541fd55d117261c415aaa8ea4
Aarushi Gupta <54365212+Aarushi5g@users.noreply.github.com>
2020-10-28 09:58:07 +0530

    Added assertEquals to StatementsTest.java

--- src/test/java/com/ordestiny/tdd/conditional/StatementsTest.java
+++ src/test/java/com/ordestiny/tdd/conditional/StatementsTest.java
@@ -1,29 +1,31 @@
 package com.ordestiny.tdd.conditional;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StatementsTest {
     private Statements object = new Statements();
 
     @Test
     public void ifElseIfTest() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number between 10 and 99999 whose no. of digits is to be found: ");
-        int input = sc.nextInt();
-        object.ifElseIf(input);
+        //number between 10 and 99999 whose no. of digits is to be found
+        assertEquals(object.ifElseIf(20), 2);
+        assertEquals(object.ifElseIf(100), 3);
+        assertEquals(object.ifElseIf(165), 3);
+        assertEquals(object.ifElseIf(9876), 4);
+        assertEquals(object.ifElseIf(90909), 5);
+        assertEquals(object.ifElseIf(24567), 5);
     }
 
     @Test
     public void switchStatementTest() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        Double input1 = sc.nextDouble();
-        System.out.println("Enter second number: ");
-        Double input2 = sc.nextDouble();
-        System.out.println("Choose an operator: +, -, *, or /: ");
-        char operator = sc.next().charAt(0);
-        object.switchStatement(input1, operator, input2);
+        assertEquals(object.switchStatement(10, '+', 20), 30);
+        assertEquals(object.switchStatement(1234, '-', 891), 343);
+        assertEquals(object.switchStatement(17, '*', 987), 16779);
+        assertEquals(object.switchStatement(59865, '/', 5), 11973);
+        assertEquals(object.switchStatement(1098, '+', 4567), 5665);
+        assertEquals(object.switchStatement(1150, '*', 2), 2300);
     }
 }
