1fec794e929254b4902abafe6ec5e2983a4ef93f
OwenYang <coolsealtw@hotmail.com>
2021-01-27 23:30:11 +0800

    add echocontroller  post api

--- src/test/java/com/ordestiny/tdd/controller/EchoControllerTest.java
+++ src/test/java/com/ordestiny/tdd/controller/EchoControllerTest.java
@@ -1,18 +1,20 @@
 package com.ordestiny.tdd.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ordestiny.tdd.service.EchoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,13 +30,24 @@ class EchoControllerTest {
 
     @BeforeEach
     void setUp() {
-        BDDMockito.given(service.echo("John")).willReturn("John");
+        given(service.echo("John")).willReturn("John");
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
     void echo() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/echo/John"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("John"));
+    }
+
+    @Test
+    void echoWithObject() throws Exception {
+        EchoController.EchoObject echoObject = new EchoController.EchoObject();
+        echoObject.setMessage("John");
+        ObjectMapper mapper = new ObjectMapper();
+        String echoJsonObject = mapper.writeValueAsString(echoObject);
+        mockMvc.perform(MockMvcRequestBuilders.post("/echo").contentType(MediaType.APPLICATION_JSON).content(echoJsonObject))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("John"));
     }
