0cba8b59e328e52cebb4e610ec37fd1cf57a694f
OwenYang <coolsealtw@hotmail.com>
2021-02-01 00:39:25 +0800

    issue-129: as a developer , i would like to validate my input add usercontroller , User object and integration test for UserController

---
+++ src/main/java/com/ordestiny/tdd/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.ordestiny.tdd.controller;
+
+import com.ordestiny.tdd.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+public class UserController {
+
+    @PostMapping("/user")
+    ResponseEntity<User> addUser(@Valid @RequestBody User user) {
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+}
